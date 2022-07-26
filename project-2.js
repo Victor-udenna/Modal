c9e9d703250bac1d3727528fd33cce75c3d938bf
victorudenna@gmail.com <victorudenna@gmail.com>
2022-07-26 16:19:47 +0100

    Added comments

--- project-2.js
+++ project-2.js
@@ -1,19 +1,26 @@
 let openBtn = document.getElementById('open-btn');
 let modalContainer = document.getElementById('modal-container');
 let closeBtn = document.getElementById('close-btn')
-
+// calls element inthe html dom
 
 openBtn.addEventListener('click', function () {
     modalContainer.style.display = 'block'; 
 })
 
+// displays the modal on click
+
 closeBtn.addEventListener('click', function () {
     modalContainer.style.display = 'none';
 })
 
+// close the button on click of the "x" icon.
 
 window.addEventListener('click', function(e) {
     if (e.target === modalContainer) {
         modalContainer.style.display = 'none';
     }
+
+    // closes button with the click of the body
 });
+
+
